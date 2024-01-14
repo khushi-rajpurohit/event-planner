9e610d797323839fdff670ae2364dda1f0eee033
Khushi Rajpurohit <rajpurohitkhushi36@gmail.com>
2024-01-14 14:56:45 +0530

    adding events and home page files

--- event-planners/src/App.js
+++ event-planners/src/App.js
@@ -1,17 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import './App.css';
-import Header from './header';
 import Home from './home';
+import Events from './events';
+import Header from './header';
 
 function App() {
   return (
-    <div className="App">
-      
-      <Header></Header>
-      <br></br>
-      <Home></Home>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/events" element={<Events />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
